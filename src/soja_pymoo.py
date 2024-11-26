bd18690fb7120e3828d28b47846714f56b7132e2
Alberto Tonda <alberto.tonda@gmail.com>
2024-11-26 22:14:48 +0100

    Normalize objectives, save population

--- src/soja_pymoo.py
+++ src/soja_pymoo.py
@@ -15,6 +15,7 @@ import sys
 # imports from pymoo
 from pymoo.algorithms.moo.moead import MOEAD
 from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.core.callback import Callback
 from pymoo.core.problem import Problem
 from pymoo.optimize import minimize
 from pymoo.termination.default import DefaultMultiObjectiveTermination
@@ -30,8 +31,13 @@ class MultiObjectiveSojaProblem(Problem) :
     fitness_functions = []
     fitness_function_args = {}
     max_soja_production = 0.0
+    max_std_soja = 0.0
+    max_total_surface = 0.0
+    normalize_objectives = True
     
-    def __init__(self, n_variables, fitness_functions, model_predictions, max_cropland_area, max_soja_production=0.0) :
+    def __init__(self, n_variables, fitness_functions, model_predictions, max_cropland_area, 
+                 max_soja_production=0.0, max_std_soja=0.0, max_total_surface=0.0,
+                 normalize_objectives=True) :
         # xl and xu are the lower and upper bounds for each variable
         super().__init__(n_var=n_variables, n_obj=len(fitness_functions), xl=0.0, xu=1.0)
         # also keep track of the fitness functions, used later during the evaluation
@@ -39,6 +45,8 @@ class MultiObjectiveSojaProblem(Problem) :
         self.fitness_function_args["model_predictions"] = model_predictions
         self.fitness_function_args["max_cropland_area"] = max_cropland_area
         self.max_soja_production = max_soja_production
+        self.max_std_soja = max_std_soja
+        self.max_cropland_area = max_total_surface
         
     def _evaluate(self, x, out, *args, **kwargs) :
         # this evaluation function starts from the assumption that 'x' is actually
@@ -50,19 +58,33 @@ class MultiObjectiveSojaProblem(Problem) :
         for i in range(0, x.shape[0]) :
             mean_soja, std_soja, total_surface = fitness_function(x[i], self.fitness_function_args)
             
-            # fitness function list
+            # fitness values list
             x_fitness_values = []
-            if "mean_soja" in self.fitness_functions :
-                # one way of turning the problem of maximizing yield into a minimization
-                # problem is by turning the result negative;
-                # x_fitness_values.append(-1 * mean_soja)
-                # another way is to minimize the difference between the maximum
-                # production available and the current production
-                x_fitness_values.append(self.max_soja_production - mean_soja)
-            if "std_soja" in self.fitness_functions :
-                x_fitness_values.append(std_soja)
-            if "total_surface" in self.fitness_functions :
-                x_fitness_values.append(total_surface)
+            
+            if self.normalize_objectives == True :
+                # this is a variant where the values of the objectives are internally
+                # normalized, so that maybe NSGA-II will give them the same importance
+                if "mean_soja" in self.fitness_functions :
+                    normalized_mean_soja = (self.max_soja_production - mean_soja) / self.max_soja_production
+                    x_fitness_values.append(normalized_mean_soja)
+                if "std_soja" in self.fitness_functions :
+                    normalized_std_soja = std_soja / self.max_std_soja
+                    x_fitness_values.append(normalized_std_soja)
+                if "total_surface" in self.fitness_functions :
+                    normalized_total_surface = total_surface / self.max_cropland_area
+                    x_fitness_values.append(normalized_total_surface)
+            else :
+                if "mean_soja" in self.fitness_functions :
+                    # one way of turning the problem of maximizing yield into a minimization
+                    # problem is by turning the result negative;
+                    # x_fitness_values.append(-1 * mean_soja)
+                    # another way is to minimize the difference between the maximum
+                    # production available and the current production
+                    x_fitness_values.append(self.max_soja_production - mean_soja)
+                if "std_soja" in self.fitness_functions :
+                    x_fitness_values.append(std_soja)
+                if "total_surface" in self.fitness_functions :
+                    x_fitness_values.append(total_surface)
                 
             # convert list to vector, place it in the appropriate place
             fitness_values[i,0,:] = np.array(x_fitness_values)
@@ -71,8 +93,44 @@ class MultiObjectiveSojaProblem(Problem) :
         out["F"] = fitness_values
         
         return
+    
+# this is a class inheriting from Callback, to save the population every few generations;
+# the algorithm will be set so that the Callback method 'notify' is invoked at the end
+# of every generation
+class SavePopulationCallback(Callback) :
+    
+    folder = ""
+    population_file_name = ""
+    generational_interval = 100
+    
+    # class constructor
+    def __init__(self, folder, population_file_name, generational_interval=100,
+                 overwrite_file=False) :
+        super().__init__()
+        self.folder = folder
+        self.population_file_name = population_file_name
+        self.generational_interval = 100
         
-
+    # this method is called at every iteration of the algorithm
+    def notify(self, algorithm) :
+        
+        # get the current generation and other information
+        generation = algorithm.n_gen
+        
+        if generation % self.generational_interval == 0 :
+            X = algorithm.pop.get("X")
+            F = algorithm.pop.get("F")
+            
+            results_dictionary = dict()
+            results_dictionary["generation"] = [generation] * X.shape[0]
+            for i in range(0, F.shape[1]) :
+                results_dictionary["fitness_%d" % (i+1)] = F[:,i]
+            for i in range(0, X.shape[1]) :
+                results_dictionary["variable_%d" % i] = X[:,i]
+            df_results = pd.DataFrame.from_dict(results_dictionary)
+            df_results.to_csv(os.path.join(self.folder, self.population_file_name + "-%d.csv" % generation), index=False)
+            
+        
 def main() :
     
     # hard-coded values
@@ -83,6 +141,7 @@ def main() :
     fitness_names = ["mean_soja", "std_soja"]
     n_objectives = len(fitness_names)
     algorithm_class = NSGA2 # it can also be NSGA2
+    seed_initial_population = True
     
     # hyperparameters for MOEA/D and NSGA2
     population_size = 1000
@@ -122,30 +181,47 @@ def main() :
     # multiple ways of knowing this, but the quick-and-dirty one is to just initialize
     # an invididual to all [1.0, 1.0,..., 1.0], and run a fitness evaluation
     max_soja_production_individual = np.ones((n_variables,))
-    max_soja_production, std_soja, total_surface = fitness_function(max_soja_production_individual,
+    max_soja_production, max_std_soja, max_total_surface = fitness_function(max_soja_production_individual,
                                     {"model_predictions" : model_predictions,
                                      "max_cropland_area" : max_cropland_area})
     
     # initialize the problem instance with all the information gathered so far
     problem = MultiObjectiveSojaProblem(n_variables, fitness_names, 
                                         model_predictions, max_cropland_area, 
-                                        max_soja_production)
+                                        max_soja_production=max_soja_production,
+                                        max_std_soja=max_std_soja,
+                                        max_total_surface=max_total_surface,
+                                        )
+    
+    # prepare an instance of the Callback class that will be used to save the population
+    callback = SavePopulationCallback(results_folder, "population-generation")
+    
+    # prepare the initial population; if population seeding has been specified,
+    # proceed accordingly
+    np.random.seed(seed=random_seed)
+    initial_population = np.random.random_sample(size=(population_size, n_variables))
+    if seed_initial_population == True :
+        initial_population[0] = max_soja_production_individual
+        initial_population[1] = np.zeros((n_variables,)) # min surface and standard deviation
     
     # set up the algorithm, depending on the class selected
     algorithm = None
     if algorithm_class is MOEAD :
         # set up the algorithm, with the reference directions
         ref_dirs = get_reference_directions("uniform", n_objectives, n_partitions=n_partitions)
-        algorithm = algorithm_class(ref_dirs, n_neighbors=n_neighbors, prob_neighbor_mating=prob_neighbor_mating)
+        algorithm = algorithm_class(ref_dirs, n_neighbors=n_neighbors, 
+                                    prob_neighbor_mating=prob_neighbor_mating,
+                                    sampling=initial_population)
     
     elif algorithm_class is NSGA2 :
-        algorithm = algorithm_class(pop_size=population_size)
+        algorithm = algorithm_class(pop_size=population_size, sampling=initial_population)
     
     # start the run
     result = minimize(problem, 
                       algorithm, 
                       #termination, # computing the convex hull takes too much time 
                       ('n_gen', max_generations), 
+                      callback=callback,
                       seed=random_seed, 
                       verbose=True)
     
